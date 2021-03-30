04332bb3b56818c7019d37939ec7fb4645d85ec0
Simranjit Kaur <sk6741486@gmail.com>
2021-03-31 09:32:38 +1300

    changes done

--- SheepRace/Factory.cs
+++ SheepRace/Factory.cs
@@ -6,7 +6,7 @@
         public static Punter GetAPunter(int id)
         {
             switch (id)
-            {
+            { 
                 case 0: return new Ali();
                 case 1: return new JoJo();
                 case 2: return new Moto();
